30d5f3ce8e2bd1978cfc81335cc236833e889cc8
John Komninos <johnkomninos@macbook-air.myfiosgateway.com>
2022-04-29 12:27:19 -0400

    updating before lunch

--- projects.js
+++ projects.js
@@ -1,13 +1,12 @@
-$(( =>{
+$(() => {
+$('.project').eq(1).hide()
+$('.project').eq(2).hide()
+$('.project').eq(3).hide()
+$('.project').eq(4).hide()
 
-
-
-
-
-
-
-
-
-
-
+for(let i = 0;i<5;i++){
+  $('.project').eq(0).on('click' , ()=>{
+    $('.project').eq(i).slideDown(1000)
+  }
+)}
 })
